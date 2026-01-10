2cbb14d72584a53a4066a36ca1dc27dc98ab5360
mangostickyricemoon-MSMOONo <mangostickyricemoon@gmail.com>
2026-01-10 08:23:07 +0700

    Updates

--- assets/basic-C-CB1--u.js
+++ assets/basic-C-CB1--u.js
@@ -1,4 +1,4 @@
-import{X as d,a as W,c,Y as v,r as y,a0 as Un,a1 as Fn,Z as Ne,a2 as Ht,a3 as qt,i as mn}from"./index-D0xqYMuB.js";import{L,O as k,P as Z,G as P,c as T,V as j,v as R,s as E,a as de,E as Y,R as B,$ as ze,a0 as wn,Q as D,a1 as wt,a2 as je,C as oe,T as sn,X as jt,q as De,M as bn,D as yn,a3 as Vn}from"./core-Dqi-IExR.js";import{c as x,U as re}from"./index-BsPzUK8M.js";const Kn=L`
+import{X as d,a as W,c,Y as v,r as y,a0 as Un,a1 as Fn,Z as Ne,a2 as Ht,a3 as qt,i as mn}from"./index-Fr18pRrS.js";import{L,O as k,P as Z,G as P,c as T,V as j,v as R,s as E,a as de,E as Y,R as B,$ as ze,a0 as wn,Q as D,a1 as wt,a2 as je,C as oe,T as sn,X as jt,q as De,M as bn,D as yn,a3 as Vn}from"./core-tJJCEhQW.js";import{c as x,U as re}from"./index-Az9NIZC6.js";const Kn=L`
   :host {
     position: relative;
     background-color: ${({tokens:e})=>e.theme.foregroundTertiary};
